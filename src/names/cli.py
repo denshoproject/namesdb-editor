6f5efd5a3b6e16cc2621e301012fa5ad739618e7
Geoffrey Jost <geoffrey@jostwebwerks.com>
2021-02-17 15:38:39 -0800

    cli: Export command for removing Django-specific tables from DB

--- src/names/cli.py
+++ src/names/cli.py
@@ -2,9 +2,15 @@ DESCRIPTION = """"""
 
 HELP = """
 Sample usage:
+
+    # Export data without Django-specific tables
+    $ namesdb export
 """
 
+from datetime import datetime
 import os
+import shutil
+import sqlite3
 import sys
 
 import click
@@ -31,3 +37,36 @@ def help():
     """Detailed help and usage examples
     """
     click.echo(HELP)
+
+@namesdb.command()
+@click.option('--debug','-d', is_flag=True, default=False)
+def export(debug):
+    """Copy SQLite3 database, removing Django-specific tables
+    """
+    src = settings.DATABASES['names']['NAME']
+    timestamp = datetime.now().strftime('%Y%m%d-%H%M')
+    dst = os.path.join(
+        os.path.dirname(src), f'namesregistry-{timestamp}.sqlite3'
+    )
+    # copy database file
+    if debug:
+        click.echo('Copying database')
+        click.echo(f'cp {src} {dst}')
+    shutil.copy(src,dst)
+    # open copied db with sqlite3
+    c = sqlite3.connect(dst).cursor()
+    if debug:
+        click.echo('Getting table names')
+    schema = c.execute("SELECT name FROM sqlite_master WHERE type='table';").fetchall()
+    if debug:
+        click.echo('Dropping Django tables')
+    for t in schema:
+        tablename = t[0]
+        if not (tablename.startswith('names_') or tablename.startswith('sqlite_')):
+            query = f'DROP TABLE {tablename}'
+            if debug:
+                click.echo(query)
+            c.execute(query)
+    # clean up
+    c.close()
+    click.echo(dst)
