5b6eaabc2a29eeed8db39b60021428e3bf61f851
Geoffrey Jost <geoffrey@jostwebwerks.com>
2022-05-24 13:44:42 -0700

    Add base template from django.contrib.admin for public UI

--- src/editor/settings.py
+++ src/editor/settings.py
@@ -108,7 +108,7 @@ INSTALLED_APPS = [
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
