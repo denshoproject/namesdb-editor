40975a51cdf817ecbe350f92697740e3fb4214d9
Geoffrey Jost <geoffrey@jostwebwerks.com>
2022-06-21 09:26:13 -0700

    cli: Print out noidminter settings in `namesdb conf`

--- src/names/cli.py
+++ src/names/cli.py
@@ -84,13 +84,14 @@ def conf():
     """
     click.echo(f'namesdb will use the following settings:')
     click.echo(f'CONFIG_FILES:            {settings.CONFIG_FILES}')
-    click.echo('')
     click.echo(f"DATABASES[names]NAME     {settings.DATABASES['names']['NAME']}")
-    click.echo('')
     click.echo(f'NAMESDB_HOST:            {settings.DOCSTORE_HOST}')
     click.echo(f'DOCSTORE_SSL_CERTFILE:   {settings.DOCSTORE_SSL_CERTFILE}')
     click.echo(f'DOCSTORE_USERNAME:       {settings.DOCSTORE_USERNAME}')
     click.echo(f'DOCSTORE_PASSWORD:       {settings.DOCSTORE_PASSWORD}')
+    click.echo(f'NOIDMINTER_URL:          {settings.NOIDMINTER_URL}')
+    click.echo(f'NOIDMINTER_USERNAME:     {settings.NOIDMINTER_USERNAME}')
+    click.echo(f'NOIDMINTER_PASSWORD:     {settings.NOIDMINTER_PASSWORD}')
     click.echo('')
 
 @namesdb.command()
