df01486dcba448735ff1ad26ff0435990ee89381
Geoffrey Jost <geoffrey@jostwebwerks.com>
2022-10-12 14:56:09 -0700

    cli: Add `namesdb searchmulti` to help match DDR names w this DB

---
+++ src/names/batch.py
@@ -0,0 +1,85 @@
+import csv
+from collections import namedtuple
+from pathlib import Path
+
+from django.conf import settings
+from django.db import connections
+
+from elastictools import search
+from . import docstore
+from namesdb_public import models as models_public
+
+
+def search_multi(csvfile, prep_names, search, click):
+    """Consume output of `ddrnames export` suggest Person records for each name
+    """
+    with Path(csvfile).open('r') as f:
+        dialect = csv.Sniffer().sniff(f.read(1024))
+        f.seek(0)
+        for row in csv.reader(f, dialect):
+            oid,fieldname,names = row
+            for n,preferred_name,nr_id,score in search(prep_names(names)):
+                click.echo(
+                    f'"{oid}", "{names}", {n}, "{preferred_name}", "{nr_id}", {score}'
+                )
+
+def prep_names_wildcard(names):
+    """Surround each name word with wildcards e.g. "*yasui* *sachi*"."""
+    return ' '.join([f'*{name}*' for name in names.replace(',', '').split(' ')])
+
+def prep_names_simple(names):
+    """Just remove all punctuation e.g. "yasui sachi"."""
+    return names.replace(',', '').replace('.', '')
+
+def fulltext_search_elastic(names, limit=25):
+    """Elasticsearch fulltext search for names in namesdb_public
+    @returns for n,preferred_name,nr_id,score for each row
+    """
+    searcher = search.Searcher(docstore.Docstore(
+        models_public.INDEX_PREFIX, settings.DOCSTORE_HOST, settings
+    ))
+    searcher.prepare(
+        params={'fulltext': names},
+        params_whitelist=['fulltext'],
+        search_models=['namesperson'],
+        sort=[],
+        fields=models_public.SEARCH_INCLUDE_FIELDS_PERSON,
+        fields_nested=[],
+        fields_agg=models_public.AGG_FIELDS_PERSON,
+        wildcards=True,
+    )
+    return [
+        (n, h.preferred_name, h.nr_id, h.meta.score)
+        for n,h in enumerate(searcher.execute(limit, 0).objects)
+    ]
+
+def fulltext_search_datasette(names, limit=25):
+    """Datasette fulltext search for names in namesdb_public
+    @returns for n,preferred_name,nr_id,score for each row
+    """
+    with connections['names'].cursor() as cursor:
+        # TODO isn't this what JOINs are for?
+        # inner query, gets rowids and ranks
+        cursor.execute(f'SELECT rowid, rank FROM names_person_fts("{names}")')
+        inner_rows = _namedtuplefetchall(cursor)
+        rowids = ','.join([str(row.rowid) for row in inner_rows])
+        ranks = {row.rowid: row.rank for row in inner_rows}
+        # outer query, gets fields
+        cursor.execute(f'SELECT rowid, * FROM names_person_fts WHERE rowid IN ({rowids})')
+        outer_rows = _namedtuplefetchall(cursor)
+        # add rank to outer query data
+        persons = [
+            [row.preferred_name, row.nr_id, ranks[row.rowid]] for row in outer_rows
+        ]
+        # sort by score and add index
+        return [
+            [n] + person for n,person in enumerate(
+                sorted(persons, key=lambda x: x[-1])
+            )
+        ]
+
+def _namedtuplefetchall(cursor):
+    """Return all rows from a cursor as a namedtuple"""
+    desc = cursor.description
+    nt_result = namedtuple('Result', [col[0] for col in desc])
+    return [nt_result(*row) for row in cursor.fetchall()]
--- src/names/cli.py
+++ src/names/cli.py
@@ -52,6 +52,7 @@ from django.conf import settings
 import requests
 from tqdm import tqdm
 
+from . import batch
 from . import csvfile
 from . import docstore
 from . import fileio
@@ -450,6 +451,26 @@ def delete(hosts, model, record_id):
         result = record.delete(using=ds.es)
     except docstore.NotFoundError as err:
         click.echo(err)
+
+@namesdb.command()
+@click.option('--preproc','-p', default='wildcard')
+@click.option('--datasette','-d', is_flag=True, default=False)
+@click.option('--elastic','-e', is_flag=True, default=True)
+@click.argument('csvfile')
+def searchmulti(preproc, datasette, elastic, csvfile):
+    """Consume output of `ddrnames export` suggest Person records for each name
+    
+    Returns: ddr_id, name_text, match_name, match_nrid, match_score
+    """
+    if preproc == 'wildcard':
+        prep_names = batch.prep_names_wildcard
+    elif preproc == 'simple':
+        prep_names = batch.prep_names_simple
+    if elastic and not datasette:
+        search = batch.fulltext_search_elastic
+    elif datasette:
+        search = batch.fulltext_search_datasette
+    batch.search_multi(csvfile, prep_names, search, click)
 
 @namesdb.command()
 @click.option('--debug','-d', is_flag=True, default=False)
