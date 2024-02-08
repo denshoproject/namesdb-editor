b9b1248419db82755128e8dbb18e7de453a48bd3
Geoffrey Jost <geoffrey.jost@densho.org>
2024-02-08 13:47:13 -0800

    noidminter: Follow redirects when getting new NOID

--- src/names/noidminter.py
+++ src/names/noidminter.py
@@ -8,11 +8,11 @@ def get_noids(num_ids=1):
     r = httpx.post(
         settings.NOIDMINTER_URL,
         data={'num': num_ids},
-        auth=(settings.NOIDMINTER_USERNAME, settings.NOIDMINTER_PASSWORD)
+        auth=(settings.NOIDMINTER_USERNAME, settings.NOIDMINTER_PASSWORD),
+        follow_redirects=True,
     )
     if r.status_code != HTTPStatus.OK:
         raise Exception(
-            f'Could not get NOID from {settings.NOIDMINTER_URL}: ' \
-            f'{r.status_code} {r.reason}'
+            f'Could not get NOID from {settings.NOIDMINTER_URL}: {r}'
         )
     return r.json()
